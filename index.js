16e42f60e630e30b9d37e5414916a62c8de00b0b
Jaap Bakker <hello@bakkerlabs.nl>
2016-08-29 18:13:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var express = require('express')
+var path = require('path')
+var parser = require('body-parser')
+var nodemailer = require('nodemailer')
+
+var app = express()
+
+app.set('views', path.join(__dirname, 'public/views'))
+app.set('view engine', 'jade')
+
+app.use('public', express.static('public'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use(parser.json())
+app.use(parser.urlencoded({extended: false}))
+
+app.listen(1337)
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+console.log('Server is running on port 1337...')
