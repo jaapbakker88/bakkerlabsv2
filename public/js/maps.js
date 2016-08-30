6f5ab002c194a7008ee417b6328df46a3c3732a5
Jaap Bakker <hello@bakkerlabs.nl>
2016-08-30 14:56:02 +0200

    aded social icons and extra backgroun doption

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,16 +1,22 @@
 var map;
       function initMap() {
 
-        var myLatLng = {lat: 40.693067, lng: -73.917881};
+        var newYork = {lat: 40.693067, lng: -73.917881};
+        var amsterdam = {lat: 52.373690, lng: 4.886247};
 
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: myLatLng,
+          center: amsterdam,
           zoom: 10,
           disableDefaultUI: true,
+          scrollwheel: false,
+          navigationControl: false,
+          mapTypeControl: false,
+          scaleControl: false,
+          draggable: false
         });
 
         var marker = new google.maps.Marker({
-          position: myLatLng,
+          position: amsterdam,
           map: map,
           title: 'Current location'
         });
