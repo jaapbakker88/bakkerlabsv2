fbfa1b999a2308b9db559b496b301b64ece587ca
Jaap Bakker <hello@bakkerlabs.nl>
2016-09-05 21:16:14 +0200

    removed failing nodemailer setup

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ var express = require('express')
 var path = require('path')
 var parser = require('body-parser')
 var nodemailer = require('nodemailer')
+var mg = require('nodemailer-mailgun-transport')
 
 var app = express()
 
 app.set('views', path.join(__dirname, 'public/views'))
 app.set('view engine', 'jade')
-
 app.use('public', express.static('public'))
-
 app.use(express.static(path.join(__dirname, 'public')))
-
-
 app.use(parser.json())
 app.use(parser.urlencoded({extended: false}))
 
 app.listen(1337)
+console.log('Server is running on port 1337...')
 
 app.get('/', function (req, res) {
   res.render('index')
 })
-
-console.log('Server is running on port 1337...')
